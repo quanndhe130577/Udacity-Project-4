ed04b65cfef765bdeefbab3ca33ba9fab4af1129
Duong Tien Loi <duongtien.loi@petronas.com>
2022-11-10 20:26:13 +0700

    update main.py

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -6,23 +6,51 @@ import socket
 import sys
 import logging
 from datetime import datetime
+from opencensus.ext.azure.log_exporter import AzureLogHandler
+from opencensus.ext.azure.log_exporter import AzureEventHandler
+from opencensus.ext.azure import metrics_exporter
+from opencensus.trace.tracer import Tracer
+from opencensus.ext.azure.trace_exporter import AzureExporter
+from opencensus.trace.samplers import ProbabilitySampler
+from opencensus.ext.flask.flask_middleware import FlaskMiddleware
 
 # App Insights
 # TODO: Import required libraries for App Insights
 
 # Logging
-logger = # TODO: Setup logger
+# # TODO: Setup logger
+logger = logging.getLogger(__name__)
+logger.addHandler(AzureLogHandler(
+    connection_string='InstrumentationKey=cb58bb52-1702-4b8a-83ec-713c58e4e386'))
+logger.addHandler(AzureEventHandler(
+    connection_string='InstrumentationKey=cb58bb52-1702-4b8a-83ec-713c58e4e386'))
 
 # Metrics
-exporter = # TODO: Setup exporter
+# # TODO: Setup exporter
+exporter = metrics_exporter.new_metrics_exporter(
+    enable_standard_metrics=True,
+    connection_string='InstrumentationKey=cb58bb52-1702-4b8a-83ec-713c58e4e386'
+)
 
 # Tracing
-tracer = # TODO: Setup tracer
+# # TODO: Setup tracer
+tracer = Tracer(
+    exporter=AzureExporter(
+        connection_string='InstrumentationKey=cb58bb52-1702-4b8a-83ec-713c58e4e386'),
+    sampler=ProbabilitySampler(1.0),
+)
 
 app = Flask(__name__)
 
 # Requests
-middleware = # TODO: Setup flask middleware
+# # TODO: Setup flask middleware
+middleware = FlaskMiddleware(
+    app,
+    # App Insights
+    exporter=AzureExporter(
+        connection_string='InstrumentationKey=cb58bb52-1702-4b8a-83ec-713c58e4e386'),
+    sampler=ProbabilitySampler(rate=1.0)
+)
 
 # Load configurations from environment or config file
 app.config.from_pyfile('config_file.cfg')
@@ -50,8 +78,11 @@ if app.config['SHOWHOST'] == "true":
     title = socket.gethostname()
 
 # Init Redis
-if not r.get(button1): r.set(button1,0)
-if not r.get(button2): r.set(button2,0)
+if not r.get(button1):
+    r.set(button1, 0)
+if not r.get(button2):
+    r.set(button2, 0)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -71,16 +102,19 @@ def index():
 
         if request.form['vote'] == 'reset':
 
-            # Empty table and return results
-            r.set(button1,0)
-            r.set(button2,0)
             vote1 = r.get(button1).decode('utf-8')
             properties = {'custom_dimensions': {'Cats Vote': vote1}}
             # TODO: use logger object to log cat vote
+            logger.warning('Cats Vote', extra=properties)
 
             vote2 = r.get(button2).decode('utf-8')
             properties = {'custom_dimensions': {'Dogs Vote': vote2}}
             # TODO: use logger object to log dog vote
+            logger.warning('Dogs Vote', extra=properties)
+
+            # Empty table and return results
+            r.set(button1, 0)
+            r.set(button2, 0)
 
             return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
 
@@ -88,7 +122,7 @@ def index():
 
             # Insert vote result into DB
             vote = request.form['vote']
-            r.incr(vote,1)
+            r.incr(vote, 1)
 
             # Get current values
             vote1 = r.get(button1).decode('utf-8')
@@ -97,8 +131,9 @@ def index():
             # Return results
             return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
 
+
 if __name__ == "__main__":
     # TODO: Use the statement below when running locally
-    app.run() 
+    app.run()
     # TODO: Use the statement below before deployment to VMSS
     # app.run(host='0.0.0.0', threaded=True, debug=True) # remote
